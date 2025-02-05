5fbe9350fd2f8f69434c137d7d3aa5b6d7fc758a
System Administrator <root@MacBook-Pro-Aleksandr.local>
2025-02-05 17:20:07 +0300

    Без задержки

--- main.cpp
+++ main.cpp
@@ -718,7 +718,7 @@ int main() {
         if (response.object) {
             example.process_response(std::move(response));
         }
-        std::this_thread::sleep_for(std::chrono::milliseconds (100));
+//        std::this_thread::sleep_for(std::chrono::milliseconds (100));
     }
 
 
